513945683aa7193f1a536b98a7438a65c6740742
Turriva <radwan3@gmail.com>
2026-05-13 17:42:46 +0000

    fix: logo 120px + navbar h-36 (144px)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -35,14 +35,14 @@ export default function Navbar() {
       }`}
     >
       <div className="container-xl">
-        <div className="flex items-center justify-between h-28">
+        <div className="flex items-center justify-between h-36">
           {/* Logo */}
           <Link to="/" className="group" style={{ textDecoration: 'none', display: 'inline-flex' }}>
             <img
               src={turrivaLogo}
               alt="Turriva"
               className="group-hover:scale-105 transition-transform"
-              style={{ height: 80, width: 'auto', maxWidth: 260 }}
+              style={{ height: 120, width: 'auto', maxWidth: 380 }}
             />
           </Link>
 
